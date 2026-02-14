ffc99667c139b41d30cbf39c125c637ffca46d17
pavel-slezak-cz <pavel.slezak@gmail.com>
2026-02-14 13:16:13 +0100

    Add package.json and eslint

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        files: ["**/*.ts"],
+        languageOptions: {
+            parserOptions: {
+                project: "./tsconfig.json",
+            },
+        },
+    },
+    {
+        ignores: ["node_modules", "dist"],
+    },
+];
